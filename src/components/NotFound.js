bf34830f3a2c6beaba8456b4d88972347e96d56b
Noah Schade <noah9@mail.com>
2019-06-03 13:44:21 -0400

    404 error page added

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="error-404-not-found">
+    <i className="material-icons icn-error">error_outline</i>
+    <h2 className="page-not-found">404 error: Page Not Found</h2>
+  </div>
+);
+
+export default NotFound;
