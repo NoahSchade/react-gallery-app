4988e85d67d2e61e79ed7f1e7a29f71e4e2ff02f
Noah Schade <noah9@mail.com>
2019-06-04 12:32:33 -0400

    Turned "Galleryitem" from a functional component to a class

--- src/components/Gallery-item.js
+++ src/components/Gallery-item.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Galleryitem = (props) => {
-  const imageItems = [];
-  [props.data][0].map((image) => {
-    return (
-      imageItems.push(
-        image.farm !== 0 ? (
-        <li key={image.id}>
-          <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt={props.subject} />
-        </li>
-        ) : '' 
+class Galleryitem extends Component {
+  render(){
+    const imageItems = [];
+    [this.props.data][0].map((image) => {
+      return (
+        imageItems.push(
+          image.farm !== 0 ? (
+          <li key={image.id}>
+            <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} alt={this.props.subject} />
+          </li>
+          ) : '' 
+        )
       )
-    )
-  });
-  
-  if(props.total === 0 && props.subject !== "cat" && props.subject !== "dog" && props.subject !== "laptop"){
-    imageItems.push(
-      // Not Found
-      <li key="Not Found" className="not-found">
-        <h3>No Results Found</h3>
-        <p>Your search did not return any results. Please try again.</p>
-      </li>
-    )
-  };
-  return imageItems;
+    });
+    
+    if(this.props.total === 0 && this.props.subject !== "cat" && this.props.subject !== "dog" && this.props.subject !== "laptop"){
+      imageItems.push(
+        // Not Found
+        <li key="Not Found" className="not-found">
+          <h3>No Results Found</h3>
+          <p>Your search did not return any results. Please try again.</p>
+        </li>
+      )
+    };
+    return imageItems;
+  }
 }
 
 export default Galleryitem;
