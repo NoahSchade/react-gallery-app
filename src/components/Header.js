11e8db282ba7d7b31bedb3b2dd2b39d1b81cf51d
Noah Schade <noah9@mail.com>
2019-06-08 14:29:30 -0400

    Added a comment in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Form from './Form';
 import Nav from './Nav';
 
+// The "Header" component contains the search bar and the default navigation buttons
 const Header = (props) => {
   return (
     <div className="header-container">
